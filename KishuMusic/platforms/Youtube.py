9030261657089769db7c2f79bbe6c54849dc950e
somushilpa <somushilpa94@gmail.com>
2024-11-14 11:40:57 +0530

    Update Youtube.py

--- KishuMusic/platforms/Youtube.py
+++ KishuMusic/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from ShilpaXMusic.utils.database import is_on_off
-from ShilpaXMusic.utils.formatters import time_to_seconds
+from KishuMusic.utils.database import is_on_off
+from KishuMusic.utils.formatters import time_to_seconds
 
 
 
